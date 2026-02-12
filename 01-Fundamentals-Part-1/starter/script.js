cdba794e16cf8b9a8d57f15873727815a7a77df3
Hamid Khanbeyki <Khanbeykihamidkhanbeyki@gmail.com>
2026-02-12 16:47:49 +0330

    variables and the way of naming them

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,23 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Reza");
+
+let firstName = "Hamid";
+// let 2years = "Ali";
+// let first&name = "Ali";
+// let function = "Ali";
+let name = "Ali";
+let Person = "Ali"; // FOR SPECIAL SETIOTATION //
+
+let NUMBER = 22233; // IT'S VALEUA WON'T CHANGE
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
