d0c0b863b0c546c4c433897d0e3a2e0716c313a5
Hamid Khanbeyki <Khanbeykihamidkhanbeyki@gmail.com>
2026-04-25 20:33:56 +0330

    The conditional operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -254,7 +254,6 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 } else {
   console.log("No one wins the trophy 😭");
 }
-*/
 const day = "monday";
 
 switch (day) {
@@ -262,35 +261,56 @@ switch (day) {
     console.log("Plan curse structure");
     console.log("Go to coding meetup");
     break;
-  case "tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-  case "thursday":
-    console.log("Write code examples");
-    break;
-  case "friday":
+    case "tuesday":
+      console.log("Prepare theory videos");
+      break;
+      case "wednesday":
+        case "thursday":
+          console.log("Write code examples");
+          break;
+          case "friday":
     console.log("Record videos");
     break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend :D");
-    break;
-  default:
-    console.log("Not a valid day!");
+    case "saturday":
+      case "sunday":
+        console.log("Enjoy the weekend :D");
+        break;
+        default:
+          console.log("Not a valid day!");
+        }
+        
+        if (day === "monday") {
+          console.log("Plan curse structure");
+          console.log("Go to coding meetup");
+        } else if (day === "tuesday") {
+          console.log("Prepare theory videos");
+        } else if (day === "wednesday" || day === "thursday") {
+          console.log("Write code examples");
+        } else if (day === "friday") {
+          console.log("Record videos");
+        } else if (day === "saturday" || day === "sunday") {
+          console.log("Enjoy the weekend :D");
+        } else {
+          console.log("Not a valid day!");
+      }
+      */
+
+const age = 18;
+
+// age >= 18
+//   ? console.log("I like to drink wine 🍷")
+//   : console.log("I like to dring water 💧");
+
+const drink = age >= 18 ? "I like you" : "I hate you";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "I like you";
+} else {
+  drink2 = "I hate you";
 }
 
-if (day === "monday") {
-  console.log("Plan curse structure");
-  console.log("Go to coding meetup");
-} else if (day === "tuesday") {
-  console.log("Prepare theory videos");
-} else if (day === "wednesday" || day === "thursday") {
-  console.log("Write code examples");
-} else if (day === "friday") {
-  console.log("Record videos");
-} else if (day === "saturday" || day === "sunday") {
-  console.log("Enjoy the weekend :D");
-} else {
-  console.log("Not a valid day!");
-}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
