63e5929f8e698d4629d1c1479c946018a091cdfb
Hamid Khanbeyki <Khanbeykihamidkhanbeyki@gmail.com>
2026-04-27 13:53:21 +0330

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -9,3 +10,23 @@ if (hasDriversLicense) console.log("I can drive :D");
 const interface = "Audio";
 const private = 534;
 const if = 'You can enter';
+*/
+
+function logger() {
+  console.log("My name is Hamid");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+console.log(fruitProcessor(5, 0));
