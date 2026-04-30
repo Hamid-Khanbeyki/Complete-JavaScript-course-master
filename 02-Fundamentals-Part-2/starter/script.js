42d3bb528e5c961d570c4b5b1e0a9d02d843bd77
Hamid Khanbeyki <Khanbeykihamidkhanbeyki@gmail.com>
2026-04-30 14:50:41 +0330

    reviewing functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -58,7 +58,6 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(2005, "Hamid"));
 
-*/
 function cutFruitPieces(fruit) {
   return fruit * 4;
 }
@@ -66,9 +65,31 @@ function cutFruitPieces(fruit) {
 function fruitProcessor(apples, oranges) {
   const applePieces = cutFruitPieces(apples);
   const orangePieces = cutFruitPieces(oranges);
-
+  
   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
   return juice;
 }
 
 console.log(fruitProcessor(2, 3));
+
+*/
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(2005, "Hamid"));
+console.log(yearsUntilRetirement(1950, "Bob"));
