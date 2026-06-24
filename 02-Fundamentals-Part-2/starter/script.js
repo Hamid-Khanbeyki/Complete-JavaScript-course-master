ead358b550da6f60c14b1ba4857a2482721d67d5
Hamid Khanbeyki <Khanbeykihamidkhanbeyki@gmail.com>
2026-06-24 16:23:29 +0330

    do fundamentals-part2's challeng 4

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -432,12 +432,11 @@ for (let excersise = 1; excersise < 4; excersise++) {
     console.log(`Exsersise ${excersise}: Lifting weight repetition ${rep}`);
   }
 }
-*/
 
 // While loop
 
 // for (let rep = 1; rep <= 10; rep++) {
-//   console.log(`Lifting the weights repetition ${rep} 🏋️‍♂️`);
+  //   console.log(`Lifting the weights repetition ${rep} 🏋️‍♂️`);
 // }
 
 let rep = 1;
@@ -453,3 +452,33 @@ while (dice !== 6) {
   dice = Math.trunc(Math.random() * 6 + 1);
   if (dice === 6) console.log("Loop is about to end...");
 }
+*/
+// challenge
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
