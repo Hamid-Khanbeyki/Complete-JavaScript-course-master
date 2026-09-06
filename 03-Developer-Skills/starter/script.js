b97d7c944a7fed65d6ed9cd050f691572deaad47
Hamid Khanbeyki <Khanbeykihamidkhanbeyki@gmail.com>
2026-09-06 16:30:32 +0330

    write temp function

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,20 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// const x = '23';
+// if (x === 23) console.log(23);
+
+// const calcAge = birthYear => 2037 - birthYear;
+
+// console.log(x);
+
+// Day 1 17c... Day 2 21c... Day 3 23c...
+const arr = [17, 21, 23];
+
+const printForecast = function (temp) {
+  for (let i = 0; i < arr.length; i++) {
+    `temp is ${i}`;
+  }
+};
+
+console.log(printForecast(arr));
